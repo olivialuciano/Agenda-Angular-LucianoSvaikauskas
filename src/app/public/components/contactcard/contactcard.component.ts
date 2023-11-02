08a2dcda1b47aa409accd1fd01dfedb78ff34066
Olivia <olivialuciano42@gmail.com>
2023-11-02 18:19:37 -0300

    volvimoss wachii

--- src/app/public/components/contactcard/contactcard.component.ts
+++ src/app/public/components/contactcard/contactcard.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ContactJsonPlaceholder } from 'src/app/interfaces/contact.interface';
+import { ContactsComponent } from '../../pages/contacts/contacts.component';
 import { ContactService } from 'src/app/services/contact.service.service';
 @Component({
   selector: 'app-contactcard',
@@ -8,9 +9,13 @@ import { ContactService } from 'src/app/services/contact.service.service';
   styleUrls: ['./contactcard.component.scss'],
 })
 export class ContactcardComponent implements OnInit {
-  constructor(private router: Router, private cs: ContactService) {}
-
-  @Output() onButtonEditClick = new EventEmitter<number>();
+  //contactsData: ContactJsonPlaceholder[] = [];
+  constructor(
+    private cc: ContactsComponent,
+    private router: Router,
+    private cs: ContactService,
+    private route: ActivatedRoute
+  ) {}
 
   @Input() contact: ContactJsonPlaceholder = {
     //recibe los datos del contacto
@@ -20,18 +25,38 @@ export class ContactcardComponent implements OnInit {
     telephoneNumber: 0,
     description: '',
   };
-  ngOnInit(): void {}
+  id: number | undefined;
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      this.id = params['id'];
+      this.getContactDetails(this.id!);
+    });
+  }
+
+  getContactDetails(id: number) {
+    this.cs.getContactDetails(id).then((r) => (this.contact = r));
+  }
 
   deleteContacto(id: number) {
     //metodo llamado desde el boton borrar del html(recibe el id del contacto de esa agenda)
     console.log('contacto id: ', id, ' eliminado');
-    this.cs.deleteContact(id); //ejecuta el metodo deleteContacto() del contact component
+    this.cc.deleteContacto(id); //ejecuta el metodo deleteContacto() del contact component
   }
 
   editContacto(id: number) {
     //metodo llamado desde el boton edit del html(recibe el id del contacto de esa agenda)
     console.log('contacto id: ', id, ' edit');
-    this.cs.editContact(id, this.contact);
-    this.onButtonEditClick.next(id);
+    this.cc.idContactoForEdit = id; //cambia el valor de idContactoForEdit en el contact component
+    this.cc.abrirContactEdit = 1;
+    //cambia el valor de abrirContactEdit en el contact component por 1 para q se cierre las contact-card y se abra el edit-form
   }
+
+  // async getContactData(id: number): Promise<void> {
+  //   this.contact = await this.cs.getContactDetails(id);
+  //   //rellena ContactData con todos los contactos de una agenda
+  // }
+  // async getData(): Promise<void> {
+  //   this.contactsData = await this.cs.getContacts(); //rellena ContactData con todos los contactos de una agenda // }
 }
--- src/app/public/pages/contacts/contacts.component.ts
+++ src/app/public/pages/contacts/contacts.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ContactJsonPlaceholder } from 'src/app/interfaces/contact.interface';
+import { AuthService } from 'src/app/services/auth.service';
 import { ContactService } from 'src/app/services/contact.service.service';
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
-  contactsData: ContactJsonPlaceholder[] = []; //lista con la cual itera el ngfor en el html
-  constructor(private cs: ContactService, private router: Router) {}
+  constructor(
+    private cs: ContactService,
+    private router: Router,
+    private auth: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
-  abrirContactEdit = false;
+  contactsData: ContactJsonPlaceholder[] = []; //lista con la cual itera el ngfor en el html
+
+  idContactoForEdit: number = 0; //este dato llega desde la contact card y es utilizado por el edit contact component
+
+  abrirContactEdit: number = 0; //0 contact card,1 editar contacto
 
   ngOnInit(): void {
     this.getData();
@@ -20,6 +30,7 @@ export class ContactsComponent implements OnInit {
   async getData(): Promise<void> {
     this.contactsData = await this.cs.getContacts(); //rellena ContactData con todos los contactos de una agenda
   }
+
   async deleteContacto(contactoId: number): Promise<void> {
     //se ejecuta desde la contact card
     await this.cs.deleteContact(contactoId);
@@ -27,20 +38,5 @@ export class ContactsComponent implements OnInit {
 
   agregarcontacto() {
     this.router.navigate(['/newcontact']);
-  }
-
-  // editContacto(id: number) {
-  //   //metodo llamado desde el boton edit del html(recibe el id del contacto de esa agenda)
-  //   console.log('contacto id: ', id, ' edit');
-  //   this.cs.idContactoForEdit = id; //cambia el valor de idContactoForEdit en el contact component
-  //   this.cs.abrirContactEdit = 1;
-  // }
-  editContacto(id: number) {
-    //metodo llamado desde el boton edit del html(recibe el id del contacto de esa agenda)
-    console.log('contacto id: ', id, ' edit');
-    this.abrirContactEdit = true;
-
-    //this.cs.idContactoForEdit = id; //cambia el valor de idContactoForEdit en el contact component
-    // cambia el valor de abrirContactEdit en el contact component por 1 para q se cierre las contact-card y se abra el edit-form
   }
 }
