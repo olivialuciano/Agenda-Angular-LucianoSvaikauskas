a9ee467e66259ff07e958d8be1c476d517f53894
Olivia Luciano <olivialuciano2003@gmail.com>
2023-11-23 21:34:18 -0300

    diseño

--- src/app/public/pages/group-detail/group-detail.module.ts
+++ src/app/public/pages/group-detail/group-detail.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { GroupDetailRoutingModule } from './group-detail-routing.module';
 import { GroupDetailComponent } from './group-detail.component';
-
+import { ContactcardComponent } from '../../components/contactcard/contactcard.component';
 
 @NgModule({
-  declarations: [
-    GroupDetailComponent
-  ],
-  imports: [
-    CommonModule,
-    GroupDetailRoutingModule
-  ]
+  declarations: [GroupDetailComponent],
+  imports: [CommonModule, GroupDetailRoutingModule],
 })
-export class GroupDetailModule { }
+export class GroupDetailModule {}
